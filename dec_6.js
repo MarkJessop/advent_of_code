0727acc4c2c90e31bf7f099ca6a8cfc5bb5e0d69
MarkJessop <markdjessop@gmail.com>
2021-12-06 21:53:44 -0500

    December 6 solution B

--- dec_6.js
+++ dec_6.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra')
+const _ = require('lodash')
 
 // a laternfish procudes a fish every 7 days
 // the fist cycle starts after 9 days
@@ -41,9 +42,50 @@ class SolutionA {
     }
 }
 
+class LanternfishColony{
+    constructor(lanternfishes){
+        this.lanternfishes = Array(9).fill(0)
+        lanternfishes.forEach(fish => {
+            this.lanternfishes[fish]++
+        })
+    }
+
+    dayTick(){
+        this.lanternfishes = this.lanternfishes.map((numberFishOnDay, daysToCreateNew, lanterfishes) => {
+            switch (daysToCreateNew){
+                case 6:
+                    return lanterfishes[daysToCreateNew + 1] + lanterfishes[0]
+                case 8:
+                    return lanterfishes[0]
+                default:
+                    return lanterfishes[daysToCreateNew + 1]
+            }
+        })
+    }
+
+    total(){
+        return _.sum(this.lanternfishes)
+    }
+}
+
+
+class SolutionB {
+    constructor(lanterfishes){
+        this.lanternfishColony = new LanternfishColony(lanterfishes)
+    }
+
+    solve() {
+        [...Array(256).keys()].forEach(day => {
+            this.lanternfishColony.dayTick()
+        })
+
+        console.log(this.lanternfishColony.total())
+    }
+}
+
 (async() => {
     let input = await (await fs.readFile('./data/dec_6_input')).toString('utf-8')
     let lanternfishes = input.split(',')
 
-    new SolutionA(lanternfishes).solve()
+    new SolutionB(lanternfishes).solve()
 })()
