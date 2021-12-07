2bb0151f19d62ce3133c96988c1860db5f0f5988
Mark Jessop <mark.jessop@shopify.com>
2021-12-07 13:27:19 -0500

    SolutionA December 7

---
+++ dec_7.js
@@ -0,0 +1,45 @@
+const fs = require('fs-extra')
+const _ = require('lodash')
+
+class SolutionA{
+  constructor(crabs){
+    this.crabs = crabs.map(crabPosition => {
+      return parseInt(crabPosition)
+    })
+    this.crabs = this.crabs.sort((a,b) => {
+      return a-b
+    })
+  }
+
+  solve(){
+    let min = _.min(this.crabs)
+    let max = _.max(this.crabs)
+
+    let position = this.findBestPosition(min, max, this.crabs)
+    console.log(position)
+  }
+
+  findBestPosition(min, max, crabList){
+    let possiblePositions = Array(max-min).fill(0)
+
+    possiblePositions = possiblePositions.map((fuel, position) => {
+      crabList.forEach(crabPosition => {
+        let fuelNeeded = Math.abs(crabPosition - position)
+        fuel += fuelNeeded
+      })
+      return fuel
+    })
+
+    let lowestFuelCost = _.min(possiblePositions)
+    return lowestFuelCost
+
+  }
+}
+
+
+(async() => {
+  let input = await (await fs.readFile('./data/dec_7_input')).toString('utf-8')
+  let crabs = input.split(',')
+
+  new SolutionA(crabs).solve()
+})()
